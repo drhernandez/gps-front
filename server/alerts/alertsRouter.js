d80f773ce49602e7ae75106b7ad770a79d491757
Diego Hernández <drhernandez92@gmail.com>
2019-08-17 17:07:21 -0300

    separated client & server

--- server/alerts/alertsRouter.js
+++ server/alerts/alertsRouter.js
@@ -1,19 +1,12 @@
+import axios from 'axios'
 var express = require('express')
 var router = express.Router()
 
-// middleware that is specific to this router
-// router.use(function timeLog(req, res, next) {
-//   console.log('Time: ', Date.now())
-//   next()
-// })
-// define the home page route
 router.put('/:id', function (req, res) {
   
-  res.send('Alerts home page')
-})
-// define the about route
-router.get('/about', function (req, res) {
-  res.send('About alerts')
+  const id = req.params.id;
+  const body = req.body;
+  
 })
 
 module.exports = router
