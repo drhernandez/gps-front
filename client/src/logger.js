9053372d340b726de28604d2509c814bad16ae29
Diego Hernández <drhernandez92@gmail.com>
2020-03-26 17:24:31 -0300

    probando logdna en prod

--- client/src/logger.js
+++ client/src/logger.js
@@ -1,15 +1,22 @@
 import Logger from 'logdna';
 
-const apiKey = process.env.REACT_APP_LOGDNA_KEY;
+const logdnaKey = process.env.REACT_APP_LOGDNA_KEY;
 const options = {
-  hostname: 'dev.gps-front.herokuapp.com',
   app: 'gps-front-client',
   env: process.env.NODE_ENV,
   failedBufRetentionLimit: 10000000, // bytes
   retryTimeout: 3000, // milliseconds
-  retryTimes: 0
+  retryTimes: 5
 }
 
-const logger = Logger.createLogger(apiKey, options);
+let __singleton;
 
-export default logger;
+const initializeSingleton = () => {
+  if (!__singleton) {
+    __singleton = process.env.NODE_ENV === 'development' ? console : Logger.createLogger(logdnaKey, options);
+  }
+
+  return __singleton;
+}
+
+export default initializeSingleton();
